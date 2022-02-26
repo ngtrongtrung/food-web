a5331b5ba738f5214280b2acbb4f1f646b86dfaf
NgTrongTrung <trung.nguyen.trong2855@gmail.com>
2022-02-26 22:31:36 +0700

    init project

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ export default {
 	roots: ["<rootDir>/src/"],
 	testMatch: ["**/+(*.)+(spec).+(ts)"],
 	setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+	moduleFileExtensions: ["ts", "html", "js", "json", "mjs"],
 	collectCoverage: true,
 	coverageReporters: ["html"],
-	coverageDirectory: "coverage/my-app",
-	moduleFileExtensions: ["ts", "html", "js", "json", "mjs"]
+	coverageDirectory: "coverage/my-app"
 };
